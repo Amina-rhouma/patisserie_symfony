0b61fcc89ff76adf234c8ce9ec82f3921fa97c3f
Amina <aminarhouma19@gmail.com>
2020-06-11 21:31:35 +0200

    update cake

--- src/Controller/CakeController.php
+++ src/Controller/CakeController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Cake;
+use App\Repository\CakeRepository;
 use App\Repository\VerrineRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Repository\RepositoryFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -24,10 +26,10 @@ class CakeController extends AbstractController
     /**
      * @Route("/produits/gateaux/{id<\d+>}", methods="get", name="productGateau")
      */
-    public function showCake(Cake $product) {
+    public function showCake(Cake $cake) {
 
         return $this->render("product/product.html.twig", [
-            'product' => $product,
+            'product' => $cake,
             'imageFolder' => $this->imageFolder
         ]);
     }
@@ -35,10 +37,10 @@ class CakeController extends AbstractController
     /**
      * @Route("/produits/gateaux/{id<\d+>}", methods="delete", name="deleteGateau")
      */
-    public function deleteCake(Cake $product, EntityManagerInterface $em): Response {
+    public function deleteCake(Cake $cake, EntityManagerInterface $em): Response {
 
         try {
-            $em->remove($product);
+            $em->remove($cake);
             $em->flush();
         } catch(\Doctrine\ORM\ORMException $exception) {
             return new Response('Could not delete this object', Response::HTTP_NOT_FOUND);
@@ -60,6 +62,7 @@ class CakeController extends AbstractController
             ->getForm()
         ;
         $form->handleRequest($request);
+
         if($form->isSubmitted() && $form->isValid())
         {
             $data = $form->getData();
@@ -92,6 +95,62 @@ class CakeController extends AbstractController
         }
         return $this->render("product/addProducts.html.twig",[
             'monFormulaire'=> $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/produits/gateaux/{id<\d+>}/modifier", methods={"GET", "POST"}, name="updateCake")
+     */
+    public function updateCake(Cake $cake, Request $request, EntityManagerInterface $em, SluggerInterface $slugger) {
+        $form = $this->createFormBuilder()
+            ->add('title', TextType::class, ['data' => $cake->getTitle()])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'data' => $cake->getDescription()
+            ])
+            ->add('price', NumberType::class, [
+                'label' => 'Prix',
+                'data' => $cake->getPrice()
+            ])
+            ->add('image', FileType::class, ['label' => 'Image'])
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            /** @var UploadedFile $file */
+            $file = $data['image'];
+            $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+            try {
+                $file->move(
+                    $this->getParameter('images_folder'),
+                    $newFilename
+                );
+            } catch (FileException $e) {
+                dd($e);
+            }
+
+            $cake->setTitle($data['title']);
+            $cake->setPrice($data['price']);
+            $cake->setDescription($data['description']);
+            $cake->setImage($newFilename);
+
+            $em->flush();
+
+            return $this->redirectToRoute("productGateau", ['id' => $cake->getId()]);
+
+        }
+
+        return $this->render("product/productUpdateForm.html.twig",[
+            'monFormulaire'=> $form->createView(),
+            'imageFolder' => $this->imageFolder,
+            'oldImage' => $cake->getImage()
         ]);
     }
 }
