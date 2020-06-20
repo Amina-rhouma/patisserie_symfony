215fc06d361717a8dddcd65328d1fc0273aa23e2
Amina <aminarhouma19@gmail.com>
2020-06-20 14:10:23 +0200

    l implementation de userinterface pour entity user cinfig de security

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -1,16 +1,26 @@
 <?php
 
 namespace App\Security;
-
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
 class LoginForm implements AuthenticatorInterface
 {
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
 
     /**
      * Does the authenticator support the given Request?
@@ -21,7 +31,7 @@ class LoginForm implements AuthenticatorInterface
      */
     public function supports(Request $request): ?bool
     {
-       return $request->attributes->get('_route') === 'app_login'
+       return $request->attributes->get('_route') === 'app_signin'
            && $request->isMethod('POST');
     }
 
@@ -40,7 +50,17 @@ class LoginForm implements AuthenticatorInterface
      */
     public function authenticate(Request $request): PassportInterface
     {
-        dd($request->getMethod());
+        $user = $this->userRepository->findOneByEmail($request->request->get('email'));
+        if (!$user) {
+            throw new CustomUserMessageAuthenticationException('Email not found');
+        }
+
+        return new Passport(
+            $user,
+            new PasswordCredentials($request->request->get('password')), [
+            new CsrfTokenBadge('login_form', $request->request->get('csrf_token'))
+        ]);
+
     }
 
     /**
@@ -71,6 +91,7 @@ class LoginForm implements AuthenticatorInterface
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         // TODO: Implement onAuthenticationSuccess() method.
+        dd('yes');
     }
 
     /**
@@ -85,5 +106,6 @@ class LoginForm implements AuthenticatorInterface
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
         // TODO: Implement onAuthenticationFailure() method.
+        dd('faild');
     }
 }
