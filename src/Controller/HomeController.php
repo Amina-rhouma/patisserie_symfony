c09d1bc2d2f2ca3c948966e197fbbb7270a5df06
Amina <aminarhouma19@gmail.com>
2020-08-19 00:14:54 +0200

    css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     private $imageFolder = "images/slider/";
+    private $imageProduit = "images/produits/";
+    private $imageInsta = "images/insta/";
+
+
+
 
     /**
      * @Route("/", name="app_home", methods={"GET"})
      */
     public function index()
     {
-        return $this->render('home.html.twig', ['imageFolder' => $this->imageFolder]);
+        return $this->render(
+            'home.html.twig',
+            [
+                'imageFolder' => $this->imageFolder,
+                'imageInsta' => $this->imageInsta,
+                'imageProduit' => $this->imageProduit,
+            ]
+        );
     }
 }
